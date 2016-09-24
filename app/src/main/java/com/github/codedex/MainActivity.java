5bc39111e29865513c50ab4e85ded357b83e35e2
fabianterhorst <fabian.terhorst@gmail.com>
2016-09-24 21:20:00 +0200

    add menu button
    * menu button will open drawer on click

--- app/src/main/java/com/github/codedex/MainActivity.java
+++ app/src/main/java/com/github/codedex/MainActivity.java
@@ -1,12 +1,18 @@
 package com.github.codedex;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.github.codedex.codeview.CodeFragment;
+import com.mikepenz.community_material_typeface_library.CommunityMaterial;
+import com.mikepenz.iconics.IconicsDrawable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +22,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(new IconicsDrawable(this, CommunityMaterial.Icon.cmd_menu).actionBar());
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
         view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 menuItem.setChecked(true);
                 drawerLayout.closeDrawers();
                 return true;
@@ -33,4 +46,15 @@ public class MainActivity extends AppCompatActivity {
                 .replace(R.id.container, codeFragment)
                 .commit();
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
