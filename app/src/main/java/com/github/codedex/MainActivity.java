e659cd4b83407b2bd5303bf95411a4017f6a6df4
fabianterhorst <fabian.terhorst@gmail.com>
2016-09-24 21:10:10 +0200

    close drawer on item click

--- app/src/main/java/com/github/codedex/MainActivity.java
+++ app/src/main/java/com/github/codedex/MainActivity.java
@@ -1,16 +1,32 @@
 package com.github.codedex;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.github.codedex.codeview.CodeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
+        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
         CodeFragment codeFragment = new CodeFragment();
         getSupportFragmentManager()
                 .beginTransaction()
