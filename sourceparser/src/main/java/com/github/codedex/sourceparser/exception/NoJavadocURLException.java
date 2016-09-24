5fa580e30a26ff5c9ab8694aff2c68baa1a577ed
ThisIsIPat <ThisIsIPat@users.noreply.github.com>
2016-09-24 20:05:04 +0200

    Introduce JavaDocWebParser and appropiate exception in case it doesn't work

---
+++ sourceparser/src/main/java/com/github/codedex/sourceparser/exception/NoJavadocURLException.java
@@ -0,0 +1,11 @@
+package com.github.codedex.sourceparser.exception;
+
+import android.nfc.FormatException;
+
+/**
+ * Created by IPat (Local) on 24.09.2016.
+ */
+
+public class NoJavadocURLException extends FormatException {
+
+}
---
+++ sourceparser/src/main/java/com/github/codedex/sourceparser/web/javadoc/JavaDocWebParser.java
@@ -0,0 +1,29 @@
+package com.github.codedex.sourceparser.web.javadoc;
+
+import com.github.codedex.sourceparser.exception.NoJavadocURLException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Created by IPat (Local) on 24.09.2016.
+ */
+
+public class JavaDocWebParser {
+    public JavaDocWebParser(URL javadocURL) throws NoJavadocURLException {
+        try {
+            Document doc = Jsoup.connect(javadocURL.toString()).get();
+            Elements packages = doc.select("ul[title=Packages]").first().children();
+            Elements classes = doc.select("frame[name=packageFrame] ul").first().children();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
