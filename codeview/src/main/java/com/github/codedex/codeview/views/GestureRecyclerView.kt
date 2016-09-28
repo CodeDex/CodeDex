1d061d4b75a1cb1cf4aa8537c9a7992175a79d19
fabianterhorst <fabian.terhorst@gmail.com>
2016-09-29 00:14:22 +0200

    cleanup gesture recyclerview

--- codeview/src/main/java/com/github/codedex/codeview/views/GestureRecyclerView.kt
+++ codeview/src/main/java/com/github/codedex/codeview/views/GestureRecyclerView.kt
@@ -4,8 +4,9 @@ import android.content.Context
 import android.support.v4.widget.ScrollerCompat
 import android.support.v7.widget.RecyclerView
 import android.util.AttributeSet
-import android.util.Log
-import android.view.*
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.Window
 import java.lang.ref.WeakReference
 
 /**
@@ -16,32 +17,18 @@ class GestureRecyclerView(context: Context, attributes: AttributeSet) : Recycler
 
     var window: WeakReference<Window>? = null
 
-    private var mDragging = false
-
-    private var mTouchSlop: Int = 0
-
     private val mScroller: ScrollerCompat = ScrollerCompat.create(context)
 
     private var gestureDetector = GestureDetector(context, this)
 
-    private var mVelocityTracker = VelocityTracker.obtain();
-
-    private var mMaximumVelocity: Int = 0
-    private var mMinimumVelocity: Int = 0
-
     init {
-        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
-        mMaximumVelocity = ViewConfiguration.get(context).getScaledMaximumFlingVelocity();
-        mMinimumVelocity = ViewConfiguration.get(context).getScaledMinimumFlingVelocity();
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         gestureDetector.onTouchEvent(event)
-        mVelocityTracker.addMovement(event)
 
         when (event.action) {
             MotionEvent.ACTION_CANCEL -> {
-                mDragging = false
                 //Stop any flinging in progress
                 if (!mScroller.isFinished) {
                     mScroller.abortAnimation()
@@ -65,19 +52,8 @@ class GestureRecyclerView(context: Context, attributes: AttributeSet) : Recycler
     }
 
     fun fling(velocityX: Int) {
-        var dif = canScroll()
-        val movingRight = velocityX > 0
-        val movingLeft = velocityX < 0
-        var currX = translationX.toInt()
-        /*if(movingLeft) {
-            currX = Math.abs(currX)
-            dif = Math.abs(dif)
-        } else if (movingRight) {
-            currX *= (-1)
-            dif *= (-1)
-        }*/
-        var maxX = -dif
-        Log.d("fling", "with translationX:" +currX.toString() + " and with vel " + velocityX.toString() + " until " + (-dif).toString())
+        val dif = canScroll()
+        val currX = translationX.toInt()
         mScroller.fling(currX, 0, velocityX, 0,
                 -dif, 0,
                 0, 0);
@@ -85,8 +61,7 @@ class GestureRecyclerView(context: Context, attributes: AttributeSet) : Recycler
 
     override fun computeScroll() {
         if (mScroller.computeScrollOffset()) {
-            var x = mScroller.currX
-            Log.d("x cc", x.toString())
+            val x = mScroller.currX
             val dif = canScroll()
             if (x < -dif) {
                 translationX = -dif.toFloat()
@@ -95,70 +70,8 @@ class GestureRecyclerView(context: Context, attributes: AttributeSet) : Recycler
             if (x > 0) {
                 return
             }
-            //x = clamp(Math.abs(x), window!!.get().decorView.width, width) * (-1)
-            Log.d("fling", "x " + x.toString())
             translationX = x.toFloat()
         }
-    }
-
-    private fun clamp(n: Int, my: Int, child: Int): Int {
-        if (my >= child || n < 0) {
-            /* my >= child is this case:
-             *                    |--------------- me ---------------|
-             *     |------ child ------|
-             * or
-             *     |--------------- me ---------------|
-             *            |------ child ------|
-             * or
-             *     |--------------- me ---------------|
-             *                                  |------ child ------|
-             *
-             * n < 0 is this case:
-             *     |------ me ------|
-             *                    |-------- child --------|
-             *     |-- mScrollX --|
-             */
-            return 0//he should get a difference
-        }
-        if (my + n > child) {
-            /* this case:
-             *                    |------ me ------|
-             *     |------ child ------|
-             *     |-- mScrollX --|
-             */
-            return child - my
-        }
-        return n
-    }
-
-    private fun clampNeg(n: Int, my: Int, child: Int): Int {
-        if (my <= child || n < 0) {
-            /* my >= child is this case:
-             *                    |--------------- me ---------------|
-             *     |------ child ------|
-             * or
-             *     |--------------- me ---------------|
-             *            |------ child ------|
-             * or
-             *     |--------------- me ---------------|
-             *                                  |------ child ------|
-             *
-             * n < 0 is this case:
-             *     |------ me ------|
-             *                    |-------- child --------|
-             *     |-- mScrollX --|
-             */
-            return 0
-        }
-        if (my - n < child) {
-            /* this case:
-             *                    |------ me ------|
-             *     |------ child ------|
-             *     |-- mScrollX --|
-             */
-            return child + my
-        }
-        return n
     }
 
     private fun canScroll(): Int {
@@ -179,16 +92,8 @@ class GestureRecyclerView(context: Context, attributes: AttributeSet) : Recycler
 
     private val SWIPE_MAX_OFF_PATH = 250
 
-    //private var ignoreFling = false
-
     override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
-        //if(!ignoreFling) {
-        Log.d("fling", "fling")
-            fling(velocityX.toInt())
-        /*    ignoreFling = true
-        } else {
-            ignoreFling = false
-        }*/
+        fling(velocityX.toInt())
         return true;
     }
 
@@ -198,7 +103,6 @@ class GestureRecyclerView(context: Context, attributes: AttributeSet) : Recycler
         val dif = canScroll()
         if (dif != 0 && x < SWIPE_MAX_OFF_PATH) {
             if (!ignore) {
-                Log.d("scroll", x.toString() + " " + y.toString())
                 if (translationX - x < -dif) {
                     translationX = -dif.toFloat()
                     return true
